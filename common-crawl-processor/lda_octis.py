ab5f7059231a867a80557c4d4f211848bb9201ab
Tanise Ceron <44325359+tceron@users.noreply.github.com>
2021-07-26 11:29:13 +0200

    Update lda_octis.py

--- common-crawl-processor/lda_octis.py
+++ common-crawl-processor/lda_octis.py
@@ -23,24 +23,27 @@ csv.field_size_limit(sys.maxsize)
 
 def load_dataset(dataset_folder):
 	dataset = Dataset()
-	dataset.load_custom_dataset_from_folder("./"+dataset_folder)
+	dataset.load_custom_dataset_from_folder(dataset_folder)
+	print(f"{dataset_folder} has been loaded successfully!")
 
 	return dataset
 
 def train_lda(dataset, out_file):
 
-	model = LDA(num_topics=100, random_state=2345)  # Create model
+	model = LDA(num_topics=100, alpha=0.1110634455531857, eta=3.415348037033642, random_state=2345)  # Create model
 	model_output = model.train_model(dataset) # Train the model
 
-	np.savez(out_file+".npz",**model_output)
+	np.savez(out_file, **model_output)
 	print(f"Output (a dictionary of array) was saved as '{out_file}'.")
+	toc = time.time()
+	print(f"It has taken {(toc-tic)/60} minutes to train the LDA model.")
 
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='Common Crawl Processor')
 
     dataset_folder = args['--pathdataset']
-    out_file = "./"+dataset_folder+"/"+args['--outputfile']
+    out_file = "./"+dataset_folder+"/"+args['--outputfile']+".npz"
 
     dataset_loaded=load_dataset(dataset_folder)
     train_lda(dataset_loaded, out_file)
