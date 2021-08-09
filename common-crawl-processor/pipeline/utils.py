971aabf39e6e0bff79f47115b13306b263f1aee9
Tanise Ceron <44325359+tceron@users.noreply.github.com>
2021-08-09 09:04:23 +0200

    Update utils.py

--- common-crawl-processor/pipeline/utils.py
+++ common-crawl-processor/pipeline/utils.py
@@ -1,5 +1,6 @@
 import json
 import os 
+from pathlib import Path
 
 def write_as_json(dic, f):
   output_file = open(f, 'w', encoding='utf-8')
@@ -11,17 +12,19 @@ def append_as_json(dic, f):
   json.dump(dic, output_file) 
   output_file.write("\n")
 
-def append_json_check_len(dic, filename, folder):
-  output_file = open(folder+filename, 'a', encoding='utf-8') 
-  if os.path.exists(folder+filename):
-    if os.path.getsize(folder+filename) < 524288000:
+def append_json_check_len(dic, filename):
+  output_file = open(filename, 'a', encoding='utf-8') 
+  if os.path.exists(filename):
+    if os.path.getsize(filename) < 524288000:
       pass
     else:
-      name, num = filename.split("_")[0], int(filename.split("_")[1])
-      filename = name+"_"+num
+      path = Path(filename)
+      f = path.parts[-1]
+      name, num = f.split("_")[0], int(f.split("_")[1].replace(".json", ""))
+      filename = name+"_"+str(num+1)+".json"
       print(filename)
       output_file.close()
-      output_file = open(folder+filename, 'a', encoding='utf-8')
+      output_file = open(str(path).replace(f, "")+filename, 'a', encoding='utf-8')
   json.dump(dic, output_file)
   output_file.write("\n")
   return filename
