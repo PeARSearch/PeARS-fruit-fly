af6fd9660a60b30dfcef5d7c03f9cac5ceed7ed6
Tanise Ceron <44325359+tceron@users.noreply.github.com>
2021-08-10 10:59:43 +0200

    Update topk_lda.py

--- common-crawl-processor/topk_lda.py
+++ common-crawl-processor/topk_lda.py
@@ -1,7 +1,7 @@
 """Common Crawl processor - retrieve the top k topics with a specific keyword associated with the documents
 
 Usage:
-  topk_lda.py --folder=<foldername> --pathdataset=<foldername> --pathmodel=<filename> --topk=<highestktopics> --word=<word>
+  topk_lda.py --folder=<foldername> --lda_path=<foldername> --model=<pathname> --topk=<highestktopics> --word=<word>
   topk_lda.py (-h | --help)
   topk_lda.py --version
 
@@ -9,9 +9,9 @@ Usage:
 Options:
   -h --help     Show this screen.
   --version     Show version.
-  --folder=<foldername>     Name of the folder where the .txt files are located
-  --pathdataset=<foldername>    Name of the folder where the preprocessed data is located  
-  --pathmodel=<filename>        Name of the file that where the LDA model has been saved
+  --folder=<foldername>      Name of the folder where the .txt files are located
+  --lda_path=<foldername>    Name of the folder where the preprocessed data is located  
+  --model=<pathname>         Name of the file that where the LDA model has been saved
   --topk=<highestktopics>       Number of topics with the highest topics associated with documents
   --word=<word>                 Word to be focused in your search of topics
 
@@ -29,38 +29,38 @@ import pandas as pd
 from docopt import docopt
 csv.field_size_limit(sys.maxsize)
 
-def load_model(filepath):
-  temp_file = datapath(filepath)
+def load_model(model):
+  temp_file = datapath(model)
   lda = LdaModel.load(temp_file, mmap='r')
   return lda
 
-def load_original_documents(folder_txt):
+def load_original_documents(folder):
   docs=[]
-  txt_ori = open(folder_txt+"/corpus_lda.txt")
+  txt_ori = open(folder+"/corpus_lda.txt")
   for line in txt_ori.read().splitlines():
     docs.append(line)
   txt_ori.close()
   return docs
 
-def load_corpus(pathdataset):
-  corpus=pickle.load(open(pathdataset+'/corpus_train.p', 'rb'))
+def load_corpus(lda_path):
+  corpus=pickle.load(open(lda_path+'/corpus_train.p', 'rb'))
   print(f"Original documents and corpus loaded...")
   return corpus
 
-def load_idx_topics(pathdataset):
+def load_idx_topics(lda_path):
   idx_topics = {}
-  txt = open(f'./{pathdataset}/topics_lda.txt', 'r')
+  txt = open(f'./{lda_path}/topics_lda.txt', 'r')
   for top in txt.read().splitlines():
     top=top.split("\t")
     idx_topics[int(top[1])]=top[0].split(", ")
   txt.close()
   return idx_topics
 
-def getktopics_prob(lda, word, k, pathdataset, folder_txt):
+def getktopics_prob(lda, word, k, lda_path, folder):
   dic={}
-  corpus=load_corpus(pathdataset)
-  idx_topics=load_idx_topics(pathdataset)
-  docs = load_original_documents(folder_txt)
+  corpus=load_corpus(lda_path)
+  idx_topics=load_idx_topics(lda_path)
+  docs = load_original_documents(folder)
   for i, doc in enumerate(docs):
     tops_text=lda.get_document_topics(corpus[i], minimum_probability=0)
     tops_text = sorted(tops_text, key=lambda x:x[1], reverse=True)
@@ -81,11 +81,11 @@ def getktopics_prob(lda, word, k, pathdataset, folder_txt):
 if __name__ == '__main__':
     args = docopt(__doc__, version='Common Crawl Processor')
 
-    folder_txt = "./"+args['--folder']
-    pathdataset = "./"+args['--pathdataset']
-    pathmodel=args['--pathmodel']
+    folder = "./"+args['--folder']
+    lda_path = "./"+args['--lda_path']
+    model=args['--model']
     k = int(args['--topk'])
     word = args['--word']
 
-    lda = load_model(pathmodel)
-    getktopics_prob(lda, word, k, pathdataset, folder_txt)
+    lda = load_model(model)
+    getktopics_prob(lda, word, k, lda_path, folder)
