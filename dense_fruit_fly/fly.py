4d58adbfb3404bcf27a6c86eed5886c5819ed2eb
minimalparts <aurelie.herbelot@cantab.net>
2022-03-08 15:10:01 +0100

    Fixed distance measure in fly

--- dense_fruit_fly/fly.py
+++ dense_fruit_fly/fly.py
@@ -27,7 +27,6 @@ class Fly:
 
         self.projections = lil_matrix(weight_mat)
         self.val_score = 0
-        self.kc_score = 1 / np.log10(int(self.kc_size * self.wta / 100))
         self.is_evaluated = False
         self.kc_use = None
         self.kc_sorted = None
@@ -52,6 +51,23 @@ class Fly:
             used_idx.extend(idx)
         return weight_mat, used_idx[:self.kc_size * proj_size]
 
+    def grow(self, num_new_rows):
+        new_mat = np.zeros((num_new_rows, self.pn_size))
+        for i in range(num_new_rows):
+            if self.init_method == "random":
+                for j in np.random.randint(self.pn_size, size=self.proj_size):
+                    new_mat[i, j] = 1
+            else:
+                random.shuffle(self.proj_store)
+                p = self.proj_store[0]
+                for j in p:
+                    new_mat[i][j] = 1
+        # concat the old part with the new part
+        self.projections = vstack([self.projections, lil_matrix(new_mat)])
+        self.projections = lil_matrix(self.projections)
+        self.kc_size+=num_new_rows
+        return self.kc_size
+   
 
     def projection_store(self,proj_store):
         weight_mat = np.zeros((self.kc_size, self.pn_size))
@@ -61,7 +77,7 @@ class Fly:
         sidx = [pn for p in self.proj_store for pn in p]
         idx = list(range(self.pn_size))
         not_in_store_idx = list(set(idx) - set(sidx))
-        print(len(not_in_store_idx),'IDs not in store')
+        #print(len(not_in_store_idx),'IDs not in store')
         used_idx = sidx.copy()
         c = 0
         
@@ -104,29 +120,28 @@ class Fly:
         if self.eval_method == "similarity":
             self.val_score, _ = self.prec_at_k(m_val=hash_val, classes_val=val_label, k=self.hyperparameters['num_nns'])
         self.is_evaluated = True
-        print("\nCOVERAGE:",self.get_coverage())
-        print("SCORE:",self.val_score)
+        #print("\nCOVERAGE:",self.get_coverage())
+        #print("SCORE:",self.val_score)
         self.kc_use = kc_use_val / np.sum(kc_use_val)
         self.kc_sorted = list(kc_sorted_val)
         #print("PROJECTIONS:",self.print_projections())
         #print("KC USE:",np.sort(self.kc_use)[::-1][:20])
         return self.val_score,self.kc_sorted[:10]
 
-    def compute_nearest_neighbours(self,cosines,labels,i,num_nns):
-        i_cos = np.array(cosines[i])
+    def compute_nearest_neighbours(self,hammings,labels,i,num_nns):
+        i_sim = np.array(hammings[i])
         i_label = labels[i]
-        ranking = np.argsort(-i_cos)
+        ranking = np.argsort(-i_sim)
         neighbours = [labels[n] for n in ranking][1:num_nns+1] #don't count first neighbour which is itself
         score = sum([1 if n == i_label else 0 for n in neighbours]) / num_nns
         #print(i,i_label,neighbours,score)
         return score,neighbours
 
     def prec_at_k(self,m_val=None,classes_val=None,k=None):
-        cosines = 1-pairwise_distances(m_val, metric="cosine") / 2 
-        #Dividing by 2 to avoid negative values. See https://stackoverflow.com/questions/37454785/how-to-handle-negative-values-of-cosine-similarities
+        hammings = 1-pairwise_distances(m_val.todense(), metric="hamming")
         scores = []
-        for i in range(cosines.shape[0]):
-            score, neighbours = self.compute_nearest_neighbours(cosines,classes_val,i,k)
+        for i in range(hammings.shape[0]):
+            score, neighbours = self.compute_nearest_neighbours(hammings,classes_val,i,k)
             scores.append(score)
         return np.mean(scores), scores
 
