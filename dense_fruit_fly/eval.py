f90faa4445c9e36de58c85a31f5788460a197c27
tceron <taniseceron@gmail.com>
2022-04-07 10:20:01 +0200

    account for multiple-label documents

--- dense_fruit_fly/eval.py
+++ dense_fruit_fly/eval.py
@@ -6,8 +6,14 @@ def compute_nearest_neighbours(vecs,labels,i,num_nns):
     i_label = labels[i]
     ranking = np.argsort(-i_sim)
     neighbours = [labels[n] for n in ranking][1:num_nns+1] #don't count first neighbour which is itself
-    score = sum([1 if n == i_label else 0 for n in neighbours]) / num_nns
-    #print(i,i_label,neighbours,score)
+    n_sum=0
+    for n in neighbours:
+        for lab in n:
+            if lab in i_label:
+                n_sum+=1
+                break
+    score = n_sum / num_nns
+    # print(i,i_label,neighbours,score)
     return score,neighbours
 
 def prec_at_k(m=None,classes=None,k=None,metric="cosine"):
